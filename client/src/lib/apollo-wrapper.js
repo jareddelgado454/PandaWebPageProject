cb0d8f5d5bb21135754524be3c7f5e922233a1f0
jareddelgado454 <jareddelgadotejada.41@gmail.com>
2024-02-24 10:34:54 -0500

    Login modified

---
+++ client/src/lib/apollo-wrapper.js
@@ -0,0 +1,33 @@
+"use client"
+import { ApolloLink, HttpLink } from '@apollo/client'
+import {
+    ApolloNextAppProvider, NextSSRApolloClient, NextSSRInMemoryCache, SSRMultipartLink
+} from '@apollo/experimental-nextjs-app-support/ssr';
+
+function makeClient()
+{
+
+    const httpLink = new HttpLink({
+        uri: "http://localhost:5000"
+    });
+
+    return new NextSSRApolloClient({
+        cache: new NextSSRInMemoryCache(),
+        link: typeof window === "undefined" ? ApolloLink.from([
+            new SSRMultipartLink({
+                stripDefer: true,
+            }),
+            httpLink
+        ]): httpLink
+            
+    });
+}
+
+export function ApolloWrapper({children})
+{
+    return <ApolloNextAppProvider
+        makeClient={makeClient}
+    >
+        {children}
+    </ApolloNextAppProvider>
+}
