d6bbc9f9e5a7476a54cf49a5d0f9f4b69a220bc9
jareddelgado454 <jareddelgadotejada.41@gmail.com>
2024-03-11 16:31:53 -0500

    last settings

--- client/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ client/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,9 +1,22 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
-    "pandacms": {
+    "pandaapi": {
       "GraphQLAPIEndpointOutput": "string",
       "GraphQLAPIIdOutput": "string",
       "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "pandaapi6c754ed6": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
     }
   }
 }
