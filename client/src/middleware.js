8323f923152104827a1816dac21d6ef561232503
jareddelgado454 <jareddelgadotejada.41@gmail.com>
2024-03-24 17:46:47 -0500

    last changes develop

---
+++ client/src/middleware.js
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server'
+ 
+const isLoggedIn = false;
+
+export function middleware(request) {
+  return NextResponse.redirect(new URL('/home', request.url))
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/about/:path*',
+}
