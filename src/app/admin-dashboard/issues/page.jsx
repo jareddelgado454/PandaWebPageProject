0048646bcae8f6aa0f85cb9aba2573c04e8f56bf
DelioCoder <davideveriwhere@gmail.com>
2024-04-23 14:44:06 -0500

    Issue's components added

--- src/app/admin-dashboard/issues/page.jsx
+++ src/app/admin-dashboard/issues/page.jsx
@@ -4,7 +4,7 @@ import CardIssue from '@/components/admin/Issues/CardIssue'
 import { client } from "@/contexts/AmplifyContext";
 import { calculateTotalPages, totalNumbers } from "@/utils/issues/CalculateIssues";
 import { getAllIssues } from "@/graphql/issues/queries/query";
-import { TableComponent } from '@/components/admin/Issues/TableComponent';
+import { IssuePagination, IssueTable } from "@/components/admin";
 function page() {
   const [page, setPage] = useState(1);
   const [rowsPerPage, setRowsPerPage] = useState(5);
@@ -18,7 +18,6 @@ function page() {
       const { data } = await client.graphql({
         query: getAllIssues,
       });
-      console.log(data);
       setIssues(data.listReports.items);
       setLoading(false);
     } catch (error) {
@@ -53,15 +52,13 @@ function page() {
     const total = calculateTotalPages(issues, rowsPerPage);
     setTotalPages(total);
   }, [issues, rowsPerPage]);
-  const disablePrevious = page === 1;
-  const disableNext = page === totalPages;
   const numbers = totalNumbers(issues);
   return (
     <div className="container mx-auto md:px-0 mb-8 slide-in-left">
       <p className="text-xl md:text-3xl text-center bg-white text-zinc-800 dark:text-white dark:bg-zinc-800 rounded-b-lg font-bold py-6 tracking-[0.1em] drop-shadow-lg transition-all">
         Issues Management
       </p>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-2 my-6 place-items-center">
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-2 my-6 place-items-center">
         {numbers.map((issue, i) => {
           return (
             <CardIssue
@@ -81,60 +78,19 @@ function page() {
       {/* Todo: Table */}
       <div className="px-4">
         {filteredIssues && (
-          <TableComponent item={filteredIssues} callback={retrieveData} />
+          <IssueTable item={filteredIssues} callback={retrieveData} />
         )}
       </div>
-      <div className="flex flex-col md:flex-row items-center justify-between w-full gap-4 px-4">
-          <div className="flex flex-row items-center mt-4 dark:text-gray-800 font-medium tab-pagination drop-shadow-lg ">
-            <button
-              disabled={disablePrevious}
-              onClick={handlePreviousPage}
-              className="rounded-l-lg border-2 bg-white text-black hover:text-white hover:bg-green-panda p-2 dark:bg-zinc-800 dark:text-white dark:border-white dark:hover:bg-green-panda dark:hover:text-black cursor-pointer transition-all"
-            >
-              Previous
-            </button>
-            {filteredIssues &&
-              Array.from({ length: totalPages }, (_, index) => (
-                <p
-                  key={index}
-                  onClick={() => handlePageClick(index + 1)}
-                  className={`p-2 ${
-                    page === index + 1
-                      ? "bg-green-panda text-white cursor-default"
-                      : "bg-white text-black hover:bg-green-panda hover:text-white cursor-pointer"
-                  } dark:bg-zinc-800 dark:text-white border-y-2 dark:hover:bg-green-panda dark:hover:text-black transition-all`}
-                >
-                  {index + 1}
-                </p>
-              ))}
-            <button
-              disabled={disableNext}
-              onClick={handleNextPage}
-              className="rounded-r-lg border-2 bg-white text-black hover:text-white hover:bg-green-panda p-2 dark:bg-zinc-800 dark:text-white dark:border-white dark:hover:bg-green-panda dark:hover:text-black cursor-pointer transition-all"
-            >
-              Next
-            </button>
-          </div>
-          {/* componente de paginación */}
-          <div className="flex gap-3 drop-shadow-lg">
-            <p className="font-bold dark:text-white text-black">
-              rows per page
-            </p>
-            <select
-              value={rowsPerPage}
-              onChange={({ target }) => setRowsPerPage(target.value)}
-              name=""
-              className="dark:text-white bg-green-panda dark:bg-black border-2 border-white rounded-lg"
-            >
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
-        </div>
+      <IssuePagination
+        page={page}
+        filteredIssues={filteredIssues}
+        totalPages={totalPages}
+        handleNextPage={handleNextPage}
+        handlePreviousPage={handlePreviousPage}
+        handlePageClick={handlePageClick}
+        rowsPerPage={rowsPerPage}
+        setRowsPerPage={setRowsPerPage}
+      />
     </div>
   )
 }
---
+++ src/components/admin/Issues/modals/Components/AnswerInput.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AnswerInput() {
+    return (
+        <div className='w-full slide-in-right overflow-hidden px-4'>
+            
+        </div>
+    )
+}
