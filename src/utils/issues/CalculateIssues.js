d0b44c17b0e885febd4f454a4e60eb5b627946e9
DelioCoder <davideveriwhere@gmail.com>
2024-04-23 14:47:18 -0500

    issue functions added

--- src/utils/issues/CalculateIssues.js
+++ src/utils/issues/CalculateIssues.js
@@ -1,12 +1,13 @@
 export const totalNumbers = (list = []) => {
     const totalIssues        = list !== null ? list.length : 0;
     const totalActiveIssues   = list !== null ? list.filter((e) => e.status === "solved").length : 0;
-    const totalInactiveIssues = list !== null ? list.filter((e) => e.status === "unsolved").length : 0;
+    const totalPendingIssues = list !== null ? list.filter((e) => e.status === "pending").length : 0;
+    const totalProcessedIssues = list !== null ? list.filter((e) => e.status === "processed").length : 0;
     return [
-        {mode: 'total', number: totalIssues}, {mode: 'solved', number: totalActiveIssues}, {
-            mode: 'unsolved',
-            number: totalInactiveIssues
-        }
+        {mode: 'total', number: totalIssues},
+        {mode: 'solved', number: totalActiveIssues},
+        {mode: 'pending', number: totalPendingIssues},
+        {mode: 'processed', number: totalProcessedIssues}
     ];
   }
 
