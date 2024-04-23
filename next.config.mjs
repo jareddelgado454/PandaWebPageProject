61b93434d87920be2aa2efd370565fdd49f06a37
DelioCoder <davideveriwhere@gmail.com>
2024-04-23 14:47:33 -0500

    Next/image domain added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        domains: ['d3nqi6yd86hstw.cloudfront.net']
+    }
+};
 
 export default nextConfig;
