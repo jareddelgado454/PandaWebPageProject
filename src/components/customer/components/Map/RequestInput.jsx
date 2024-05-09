bc553755c000eb974223f79472db29fd4cb17476
DelioCoder <davideveriwhere@gmail.com>
2024-05-09 09:15:26 -0500

    customer actions from map

--- src/components/customer/components/Map/RequestInput.jsx
+++ src/components/customer/components/Map/RequestInput.jsx
@@ -6,7 +6,7 @@ export default function RequestInput() {
     const [isInputActive, setIsInputActive] = useState(true);
     return (
         <>
-            <div className={`transtion-all duration-300 absolute ${isInputActive ? 'bottom-[7rem]' : '-bottom-6 delay-[10ms]'} left-[48%] transform -translate-y-1/2 w-full`}>
+            <div className={`transtion-all duration-300 absolute ${isInputActive ? 'bottom-[13rem]' : 'bottom-0 delay-[10ms]'} left-[48%] transform w-full`}>
                 <div className='w-[3rem] h-[3rem] bg-white dark:bg-zinc-900 rounded-t-full cursor-pointer' onClick={() => setIsInputActive(!isInputActive)}>
                     <div className='flex justify-center items-center w-full h-full'>
                         <FaArrowUp className={`transition-all duration-250 ${isInputActive ? 'rotate-180' : 'rotate-0'}`} />
@@ -14,8 +14,8 @@ export default function RequestInput() {
                 </div>
 
             </div>
-            <div className='flex items-center justify-center h-[144px]'>
-                <div className={`dark:bg-zinc-900 rounded-t-lg bg-white absolute shadow transition-all ${isInputActive ? '-bottom-0 h-[144px] duration-500' : '-bottom-6 h-0 delay-[73ms] duration-700'} w-[60%]`}>
+            <div className='flex items-center justify-center h-[220px]'>
+                <div className={`dark:bg-zinc-900 rounded-t-lg bg-white absolute shadow transition-all ${isInputActive ? '-bottom-0 h-[220px] duration-500' : '-bottom-6 h-0 delay-[73ms] duration-700'} w-[60%]`}>
                     <ServiceForm />
                 </div>
             </div>
