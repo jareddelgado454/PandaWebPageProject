ef0f6631fa42511d1a922c0418df78789322f179
DelioCoder <davideveriwhere@gmail.com>
2024-05-15 17:05:48 -0500

    DarkModeButton Changed

--- src/components/DarkModeButton.jsx
+++ src/components/DarkModeButton.jsx
@@ -21,7 +21,7 @@ export default function DarkModeButton() {
     return (
         <button
             type="button"
-            className="absolute z-50 dark:bg-zinc-900 bg-white text-zinc-600 dark:border-2 dark:border-zinc-800 dark:text-white bottom-20 right-10 rounded-full p-4 shadow-md transition-all ease-in-out hover:-translate-y-1 hover:scale-110"
+            className="absolute z-50 dark:bg-zinc-900 bg-white text-zinc-600 dark:border-2 dark:border-zinc-800 dark:text-white bottom-12 right-10 rounded-full p-4 shadow-md transition-all ease-in-out hover:-translate-y-1 hover:scale-110"
             onClick={handleChangeTheme}
         >
             <FaMoon className="text-2xl" />
