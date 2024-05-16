786c815564e6c819032ab7dca25097da97db7b8b
DelioCoder <davideveriwhere@gmail.com>
2024-05-15 19:51:49 -0500

    tailwind nextui fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
  
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   darkMode: 'class',
   theme: {
