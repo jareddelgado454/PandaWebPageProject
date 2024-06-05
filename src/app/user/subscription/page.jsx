d8bccb958d252fa48b7a16ebc14be60a11fe1075
jareddelgado454 <jareddelgadotejada.41@gmail.com>
2024-06-05 15:38:30 -0500

    last changes

--- src/app/user/subscription/page.jsx
+++ src/app/user/subscription/page.jsx
@@ -35,8 +35,8 @@ const Subscription = () => {
             console.log("El proceso fue correcto paso1",account);
             const accountLink = await stripe.accountLinks.create({
                 account: account.id,
-                refresh_url: 'http://localhost:3000/user', 
-                return_url: 'http://localhost:3000/user/subscription', 
+                refresh_url: 'https://master.d3dtglewderhtg.amplifyapp.com//user', 
+                return_url: 'https://master.d3dtglewderhtg.amplifyapp.com//user/subscription', 
                 type: 'account_onboarding', 
             });
             console.log("El proceso fue correcto paso2",accountLink);
