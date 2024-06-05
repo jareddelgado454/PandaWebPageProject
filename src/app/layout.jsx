45a0696b09966f5569f0d7e6728e47c1ced541b7
DelioCoder <davideveriwhere@gmail.com>
2024-06-05 09:52:44 -0500

    context containers added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,10 @@
 import "./globals.css";
 import AmplifyContext from "@/contexts/AmplifyContext";
 import ToastContainerComponent from "@/components/toast/ToastContainerComponent";
+import { PlaceProvider } from '@/contexts/place/PlaceProvider';
+import { MapProvider } from '@/contexts/map/MapProvider';
+import { ServiceProvider } from '@/contexts/service/ServiceProvider';
+import { UserProvider } from '@/contexts/user/UserProvider';
 export const metadata = {
   title: "The panda App",
   description: "The Panda is a mobile auto repair service CRM",
@@ -9,10 +13,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="max-h-full">
       <body className={`antialiased dark:bg-zinc-800`}>
-            <AmplifyContext>
-                <ToastContainerComponent />
-                {children}
-            </AmplifyContext>
+        <UserProvider>
+          <PlaceProvider>
+            <MapProvider>
+              <ServiceProvider>
+                <AmplifyContext>
+                  <ToastContainerComponent />
+                  {children}
+                </AmplifyContext>
+              </ServiceProvider>
+            </MapProvider>
+          </PlaceProvider>
+        </UserProvider>
       </body>
     </html>
   );
