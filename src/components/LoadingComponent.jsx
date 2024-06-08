4b46b287ee669eb1ab58349439332177bb8ea546
DelioCoder <davideveriwhere@gmail.com>
2024-06-07 19:20:04 -0500

    Spinner background removed

--- src/components/LoadingComponent.jsx
+++ src/components/LoadingComponent.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 export default function LoadingComponent() {
     return (
-        <div className='transition-all absolute w-full h-full flex justify-center items-center bg-white/35 top-0'>
+        <div className='transition-all absolute w-full h-full flex justify-center items-center top-0 overflow-hidden'>
             <div className='loader bg-green-pan' />
         </div>
     )
