6d6eef7b82a9ee159c3bc5076aae40db334a567e
DelioCoder <davideveriwhere@gmail.com>
2024-06-14 18:03:41 -0500

    charts fixed

--- src/components/chartjs/highRate/HighRateComponent.jsx
+++ src/components/chartjs/highRate/HighRateComponent.jsx
@@ -1,54 +1,51 @@
-'use client';
+import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
-import { client } from '@/contexts/AmplifyContext';
-import { filterUserByRate } from '@/graphql/users/query/user';
 import { FaFaceSmile, FaStar } from 'react-icons/fa6';
 
-function HighRateComponent() {
+function HighRateComponent({ technicians }) {
+    const [techniciansSorted, setTechniciansSorted] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [users, setUsers] = useState(null);
-    const retrieveData = async() => 
-    {
-      setLoading(true);
-  
-      try {
-        
-        const { data } = await client.graphql({
-          query: filterUserByRate
-        });
-        const sortedUsers = data.listUsers.items.sort((a, b) => b.rate - a.rate);
-        await setUsers(sortedUsers);
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
-        setLoading(false);
-      }
-    }
+
     useEffect(() => {
-        retrieveData();
-      }, []);
-    
+        if (technicians) {
+            // Filtrar y ordenar los técnicos por su calificación
+            const sortedTechnicians = technicians
+                .map(technician => ({
+                    ...technician,
+                    rate: technician.rate.items.length > 0 ? technician.rate.items[0].rate : 0
+                }))
+                .filter(technician => technician.rate > 4.5)
+                .sort((a, b) => b.rate - a.rate);
+
+            setTechniciansSorted(sortedTechnicians);
+            setLoading(false);
+        }
+    }, [technicians]);
+
     return (
         <div className='flex flex-col gap-4'>
             {
                 loading ? (<div>loading</div>) : (
                     <>
-                        {users && users.map((user, i) => (
-                            <div key={i} className='bg-white rounded-lg h-[6rem] flex flex-row gap-2 px-4'>
+                        {techniciansSorted && techniciansSorted.map((technician, i) => (
+                            <div key={i} className='bg-white dark:bg-zinc-950 rounded-lg h-[6rem] flex flex-row gap-2 px-4'>
                                 <div className='w-1/4 lg:w-2/12 flex justify-center items-center'>
-                                    <img
-                                        src={`${user.profilePicture ? user.profilePicture : "/image/defaultProfilePicture.jpg"}`}
-                                        alt="user_profile"
+                                    <Image
+                                        width={150}
+                                        height={150}
+                                        priority
+                                        src={`${technician.profilePicture ? technician.profilePicture : "/image/defaultProfilePicture.jpg"}`}
+                                        alt="technician_profile"
                                         className='rounded-full w-[2.5rem] h-[2rem] lg:w-[4rem] lg:h-[4rem]'
                                     />
                                 </div>
                                 <div className='w-full flex flex-row justify-between'>
                                     <div className='flex flex-col justify-center gap-2'>
-                                        <p className='text-black text-sm lg:text-base font-semibold text-center w-[4.5rem] lg:w-full'>{ user.fullName }</p>
+                                        <p className='text-black dark:text-white text-sm lg:text-base font-semibold text-center w-[4.5rem] lg:w-full'>{technician.fullName}</p>
                                         <div className='bg-green-panda w-[3rem] h-[1.7rem] text-sm rounded-2xl flex flex-row items-center justify-center gap-1'>
                                             <div className='flex flex-row gap-1 justify-center items-center'>
                                                 <FaStar />
-                                                { user.rate }
+                                                {technician.rate}
                                             </div>
                                         </div>
                                     </div>
@@ -57,7 +54,7 @@ function HighRateComponent() {
                                     </div>
                                 </div>
                             </div>
-                        )) }
+                        ))}
                     </>
                 )
             }
--- src/components/chartjs/index.js
+++
@@ -1,2 +0,0 @@
-export { default as LineChartGained } from './userGained/LineChart';
-export { default as DonutChartPerRole } from './perRole/DoughnutChart';
