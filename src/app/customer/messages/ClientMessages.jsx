d3cf4a164596f8c7942491fbb0d4efbb8598a123
DelioCoder <davideveriwhere@gmail.com>
2024-06-14 14:54:54 -0500

    Using params

--- src/app/customer/messages/ClientMessages.jsx
+++ src/app/customer/messages/ClientMessages.jsx
@@ -1,9 +1,34 @@
 'use client';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSearchParams } from 'next/navigation';
 import { ChatCustomerComponent, ContactsComponent } from '@/components/customer';
+import { getChatById } from '@/graphql/chat/query';
+import { client } from '@/contexts/AmplifyContext';
 export default function ClientMessages() {
+    const param = useSearchParams().get('chatId');
     const [chatActive, setChatActive] = useState(false);
     const [chatSelected, setChatSelected] = useState(null);
+    const retrieveOneChat = async(paramId) => {
+        try {
+            const { data } = await client.graphql({
+                query: getChatById,
+                variables: {
+                    chatId: paramId
+                }
+            });
+            console.log(data);
+            setChatSelected(data.getChat);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    useEffect(() => {
+        if (param) {
+            console.log(param);
+            setChatActive(true);
+            retrieveOneChat(param);
+        }
+    }, [param]);
     return (
         <div className='p-4 h-[100vh] md:h-full'>
             <div className='flex flex-col gap-4 lg:flex-row h-full'>
