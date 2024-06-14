c1ae1fd544c387c3438eda441fefb92965bad437
DelioCoder <davideveriwhere@gmail.com>
2024-06-14 14:54:36 -0500

    MessageNotificationComponent added

--- src/app/customer/layout.jsx
+++ src/app/customer/layout.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 import 'animate.css';
 import '@/app/app.css';
 import { ServiceProvider } from '@/contexts/service/ServiceProvider';
+import MessageNotification from '@/components/customer/components/MessageNotification';
 export default function layout({ children }) {
   return (
     <div className=' bg-stone-300 dark:bg-zinc-700 h-full'>
       <div className='flex flex-col lg:flex-row items-center h-full xl:h-screen px-4 py-4 lg:py-0 gap-2'>
       <ServiceProvider>
+        <MessageNotification />
         <CustomerBar />
         <div className='w-full bg-white dark:bg-zinc-800 h-full md:h-[calc(100vh-50px)] 2xl:h-[calc(100vh-100px)] shadow-lg rounded-lg relative overflow-hidden'>
           {children}
