f65e28cd2fffd56094a56b4ed87754cd46abf855
DelioCoder <davideveriwhere@gmail.com>
2024-06-14 14:59:26 -0500

    Handling buttons with service's status

--- src/components/customer/components/request/detail/TechnicianDetail.jsx
+++ src/components/customer/components/request/detail/TechnicianDetail.jsx
@@ -11,7 +11,7 @@ import { useRouter } from 'next/navigation';
 import { UserContext } from '@/contexts/user/UserContext';
 import { Button } from '@nextui-org/react';
 import { existChatWithTechnicianSelected } from '@/api/service';
-export default function TechnicianDetail({ technician }) {
+export default function TechnicianDetail({ technician, status }) {
   const { user } = useContext(UserContext);
   const router = useRouter();
   const onCreateChat = async () => {
@@ -19,7 +19,7 @@ export default function TechnicianDetail({ technician }) {
       const id = await existChatWithTechnicianSelected(user.id, technician.id);
 
       if (id) {
-        router.replace(`/customer/messages/${id}`);
+        router.push(`/customer/messages?chatId=${id}`);
       } else {
         const { data } = await client.graphql({
           query: createChat,
@@ -28,7 +28,7 @@ export default function TechnicianDetail({ technician }) {
             technicianId: technician.id
           }
         });
-        router.replace(`/customer/messages/${data.createChat.id}`);
+        router.push(`/customer/messages?chatId=${data.createChat.id}`);
       }
 
     } catch (error) {
@@ -37,7 +37,7 @@ export default function TechnicianDetail({ technician }) {
   }
   const calculateAverageRate = (items) => {
     if (items.length === 0) return 0; // Manejo de caso donde el array está vacío
-  
+
     const totalRate = items.reduce((sum, item) => sum + item.rate, 0);
     return totalRate / items.length;
   };
@@ -58,7 +58,7 @@ export default function TechnicianDetail({ technician }) {
               <p className='text-md lg:text-base 2xl:text-2xl font-semibold text-[#40C48E]'>{technician.fullName}</p>
               <p className='text-xs lg:text-sm text-zinc-500 dark:text-zinc-300'>{ThirdDateFormatter(technician.createdAt)}</p>
               <div className='flex flex-row gap-1 items-center'>
-                
+
                 {technician?.rate.items.length > 0 ? (
                   <>
                     <p className='text-xs lg:text-base'>Rate:</p>
@@ -73,7 +73,7 @@ export default function TechnicianDetail({ technician }) {
                       fullIcon={<FaRegStar />}
                       activeColor="#ffd700"
                     />
-                   <p>{calculateAverageRate(technician.rate.items)}</p>
+                    <p>{calculateAverageRate(technician.rate.items)}</p>
                   </>
                 ) : (
                   <div>No stars</div>
@@ -82,12 +82,16 @@ export default function TechnicianDetail({ technician }) {
             </div>
           </div>
           <div className='w-full lg:w-[20%] flex flex-row h-full gap-4 justify-end'>
-            <Button isIconOnly className='bg-white dark:bg-zinc-800 dark:hover:bg-green-panda transition-all duration-300 p-4 w-[3rem] h-[3rem] rounded-full shadow-lg cursor-pointer'>
-              <FaPhoneVolume className='text-lg' />
-            </Button>
-            <Button isIconOnly onClick={onCreateChat} className='bg-white dark:bg-zinc-800 dark:hover:bg-green-panda transition-all duration-300 p-4 w-[3rem] h-[3rem] rounded-full shadow-lg cursor-pointer'>
-                <FaCommentSms className='text-2xl' /> 
-            </Button>
+            {status !== "completed" && (
+              <>
+                <Button isIconOnly className='bg-white dark:bg-zinc-800 dark:hover:bg-green-panda transition-all duration-300 p-4 w-[3rem] h-[3rem] rounded-full shadow-lg cursor-pointer'>
+                  <FaPhoneVolume className='text-lg' />
+                </Button>
+                <Button isIconOnly onClick={onCreateChat} className='bg-white dark:bg-zinc-800 dark:hover:bg-green-panda transition-all duration-300 p-4 w-[3rem] h-[3rem] rounded-full shadow-lg cursor-pointer'>
+                  <FaCommentSms className='text-2xl' />
+                </Button>
+              </>
+            )}
           </div>
         </div>
       ) : (<div className='font-semibold text-2xl dark:text-zinc-400'>No technician assigned yet</div>)}
