cc0b07a097df59cea2738b39da505f24323cc615
DelioCoder <davideveriwhere@gmail.com>
2024-06-24 16:09:56 -0500

    Text removed

--- src/app/customer/service/ClientService.jsx
+++ src/app/customer/service/ClientService.jsx
@@ -37,19 +37,6 @@ export default function ClientService() {
         <RequestInput />
         <OfferDetails />
         <CancelService />
-        {/* <div className='absolute bottom-5 left-0 w-full'>
-          <div className='flex justify-between items-center px-2'>
-
-            <div className='dark:bg-zinc-800 bg-white p-4 rounded-lg w-2/4'>
-              <input
-                type="text"
-                className='dark:bg-zinc-900 bg-stone-200 p-2 w-full rounded'
-                placeholder='Search'
-              />
-            </div>
-            <p>03</p>
-          </div>
-        </div> */}
       </div>
     </div>
   )
