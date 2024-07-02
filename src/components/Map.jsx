4325ea36281ca13294d15622c04bcff054939853
DelioCoder <davideveriwhere@gmail.com>
2024-07-02 09:26:33 -0500

    using just markers

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -69,7 +69,7 @@ export default function Map({ userMarkerRef }) {
             easing: (t) => t,
           });
 
-          new Marker()
+          userMarkerRef = new Marker()
             .setLngLat(userLocation)
             .addTo(mapC);
 
