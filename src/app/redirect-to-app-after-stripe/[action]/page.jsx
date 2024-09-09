797c7deb5aa0bd71c0cdc1923ebd19cf8036c023
DelioCoder <davideveriwhere@gmail.com>
2024-09-09 09:35:02 -0500

    Redirect from stripe added

---
+++ src/app/redirect-to-app-after-stripe/[action]/page.jsx
@@ -0,0 +1,26 @@
+"use client"
+import React, { useEffect } from "react";
+import { useRouter, useParams } from "next/navigation";
+const RedirectingToAppAfterStripe = () => {
+    const router = useRouter();
+    const params = useParams();
+
+    useEffect(() => {
+        const appRedirectSuccessUrl = "panda-technician://(auth)/(complete-information)";
+        const appRedirectErrorUrl = "panda-technician://(auth)/(complete-information)/stripeaccount";
+
+        window.location.href = params.action === "completed" ?  appRedirectSuccessUrl : appRedirectErrorUrl;
+
+        setTimeout(() => {
+            window.close();
+        }, 3000); 
+    }, []);
+
+    return (
+        <div className="w-full h-screen bg-zinc-900 text-white flex items-center justify-center">
+            <img src="/loading/loading4.gif" className="w-[100px] h-[100px]" alt="Loading" />
+        </div>
+    );
+};
+
+export default RedirectingToAppAfterStripe;
