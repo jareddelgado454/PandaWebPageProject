b9ee0ec9de83f77b8a43dfc4d2a44d469a947c68
DelioCoder <davideveriwhere@gmail.com>
2024-09-23 14:26:28 -0500

    React-hotkeys added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,36 @@
+'use client';
 import React from 'react';
+import { useRouter } from 'next/navigation';
+import { HotKeys } from 'react-hotkeys';
 import { AboutSection, BannerSection, FooterSection, ServiceSection, TeamSection } from '@/components/home';
 import { FloatSidebar } from '@/components/home/FloatSidebar';
 import AskedQuestion from '@/components/home/sections/AskedQuestion';
 export default function Page() {
+    const router = useRouter();
+    const redirectToAdmin = () => {
+        router.push('/auth/signin');
+    };
+    const keyMap = {
+        REDIRECT_TO_ADMIN: 'Control+Alt+a',
+    };
+
+    const handlers = {
+        REDIRECT_TO_ADMIN: (event) => {
+            event.preventDefault();
+            redirectToAdmin();
+        },
+    };
     return (
-        <div className='flex-1 h-svh'>
-            <FloatSidebar />
-            <BannerSection />
-            <AboutSection />
-            <ServiceSection />
-            <TeamSection />
-            <AskedQuestion />
-            <FooterSection />
-        </div>
+        <HotKeys keyMap={keyMap} handlers={handlers} >
+            <div className='flex-1 h-svh'>
+                <FloatSidebar />
+                <BannerSection />
+                <AboutSection />
+                <ServiceSection />
+                <TeamSection />
+                <AskedQuestion />
+                <FooterSection />
+            </div>
+        </HotKeys>
     );
 }
