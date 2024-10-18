ab8e085af9824956610bc119e10de1484c6411cb
DelioCoder <davideveriwhere@gmail.com>
2024-10-18 17:04:45 -0500

    AddUserModal added

--- src/components/admin/modals/AddUserModal.jsx
+++ src/components/admin/modals/AddUserModal.jsx
@@ -1,168 +1,159 @@
-import React from 'react';
-import {Field, Form, Formik} from "formik";
-import {signUp} from "aws-amplify/auth";
-import {toast} from "react-toastify";
+'use client';
+import React, { useState } from 'react';
+import { Field, Form, Formik } from "formik";
+import { signUp } from "aws-amplify/auth";
+import { toast } from "react-toastify";
 import {
-    Modal, ModalHeader, ModalFooter, ModalContent, ModalBody, Button
+    Modal, ModalHeader, ModalFooter, ModalContent, ModalBody, Button,
 } from "@nextui-org/react";
-import {handleCreateUserOnDatabase} from "@/api";
-const AddUserModal = ({ isOpen, onOpenChange }) => {
+import { handleCreateUserOnDatabase } from "@/api";
+import { generateTempoPassword } from '@/utils/GenerateTempoPassword';
+const AddUserModal = ({ isOpen, onOpenChange, callback }) => {
+    const [isPassordGenerated, setIsPassordGenerated] = useState(false);
 
-    const createUserInCognito = async(values) => {
-        let isAdded = false;
-        try {
-            const signUpResponse = await signUp({
-                username: values.email,
-                password: values.password,
-                options: {
-                    userAttributes: {
-                        email: values.email,
-                        "custom:role": values.role,
-                        "custom:fullName": values.fullName,
-                        "custom:status": values.status
-                    }
-                }
-            });
-            isAdded = true;
-            return { signUpResponse, isAdded };
-        }catch (error) {
-            console.log(error);
-            toast.error("Something went wrong.");
-        }
-
-    }
-
-    const onHandleSubmit = async(values, { resetForm, setSubmitting }) => {
+    const onHandleSubmit = async (values, { resetForm, setSubmitting }) => {
 
         setSubmitting(true);
         try {
-            const { signUpResponse } = await createUserInCognito(values);
             await handleCreateUserOnDatabase({
                 fullName: values.fullName,
                 email: values.email,
-                password: values.password,
-                role: values.role,
                 status: values.status,
-                cognitoId: signUpResponse.userId
+                temporaryPassword: values.temporaryPassword
             });
-
+            setIsPassordGenerated(false);
             toast.success(`User ${values.fullName} has been created.`);
             resetForm(true);
-
-        }catch (error) {
+            callback();
+        } catch (error) {
             console.log(error);
             toast.error("Something went wrong.");
         }
+    }
+
+    const handleGeneratePassword = (setFieldValue) => {
+        const passwordGenerated = generateTempoPassword();
+        setFieldValue('temporaryPassword', passwordGenerated);
+        setIsPassordGenerated(!isPassordGenerated);
     }
 
     return (
         <Modal backdrop="blur" isOpen={isOpen} onOpenChange={onOpenChange} size="xl">
-           <ModalContent>
-               {(onClose) => (
-                   <>
-                       <ModalHeader className="flex justify-center items-center">
-                           <p className="text-center">Add New Administrator</p>
-                       </ModalHeader>
-                       <ModalBody className="">
-                           <div className={`flex flex-col lg:flex-row gap-8 justify-center w-full`}>
-                               <Formik
-                                   initialValues={{
-                                       fullName: '',
-                                       email: '',
-                                       password: '',
-                                       role: 'customer',
-                                       status: 'active',
-                                   }}
-                                   validateOnChange={false}
-                                   onSubmit={onHandleSubmit}
-                               >
-                                   {({ handleSubmit }) => (
-                                       <Form
-                                           onSubmit={handleSubmit}
-                                           className="flex flex-col gap-4 justify-center items-center w-full"
-                                       >
-                                           <div className="w-10/12">
-                                               <label
-                                                   className="block text-gray-700 text-md font-bold mb-2"
-                                                   htmlFor="fullName"
-                                               >
-                                                   fullName
-                                               </label>
-                                               <Field
-                                                   className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full"
-                                                   type="text"
-                                                   id="fullName"
-                                                   name="fullName"
-                                               />
-                                           </div>
-                                           <div className="w-10/12">
-                                               <label
-                                                   className="block text-gray-700 text-md font-bold mb-2"
-                                                   htmlFor="Email"
-                                               >
-                                                   Email
-                                               </label>
-                                               <Field
-                                                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                                   type="email"
-                                                   id="Email"
-                                                   name="email"
-                                               />
-                                           </div>
-                                           <div className="w-10/12">
-                                               <label
-                                                   className="block text-gray-700 text-md font-bold mb-2"
-                                                   htmlFor="password"
-                                               >
-                                                   Password
-                                               </label>
-                                               <Field
-                                                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight dark:text-white focus:outline-none focus:shadow-outline"
-                                                   type="password"
-                                                   id="password"
-                                                   name="password"
-                                               />
-                                           </div>
-                                           <div className="w-10/12">
-                                               <label
-                                                   className="block text-gray-700 text-md font-bold mb-2"
-                                                   htmlFor="role"
-                                               >
-                                                   Role
-                                               </label>
-                                               <Field
-                                                   id="role"
-                                                   as="select"
-                                                   name="role"
-                                                   className="block appearance-none w-full border border-gray-200 text-gray-700 dark:text-white py-3 px-4 pr-8 rounded leading-tight focus:outline-none"
-                                               >
-                                                   <option value="customer">Customer</option>
-                                                   <option value="technician">Technician</option>
-                                                   <option value="admin">Administrator</option>
-                                               </Field>
-                                           </div>
-                                           <div className="w-10/12 ">
+            <ModalContent>
+                {(onClose) => (
+                    <>
+                        <ModalHeader className="flex justify-center items-center">
+                            <p className="text-center">Add New Administrator</p>
+                        </ModalHeader>
+                        <ModalBody className="">
+                            <div className={`flex flex-col lg:flex-row gap-8 justify-center w-full`}>
+                                <Formik
+                                    initialValues={{
+                                        fullName: '',
+                                        email: '',
+                                        temporaryPassword: '',
+                                        status: 'inactive',
+                                    }}
+                                    validateOnChange={false}
+                                    onSubmit={onHandleSubmit}
+                                >
+                                    {({ handleSubmit, setFieldValue, values }) => (
+                                        <Form
+                                            onSubmit={handleSubmit}
+                                            className="flex flex-col gap-4 justify-center items-center w-full"
+                                        >
+                                            <div className="w-10/12">
+                                                <label
+                                                    className="block text-gray-700 dark:text-zinc-300 text-md font-bold mb-2 tracking-widest"
+                                                    htmlFor="fullName"
+                                                >
+                                                    fullName
+                                                </label>
+                                                <Field
+                                                    className="bg-zinc-100 shadow appearance-none border rounded-xl h-[40px] py-2 px-3 text-gray-700 dark:text-zinc-200 leading-tight focus:outline-none focus:shadow-outline w-full dark:bg-zinc-800 dark:border-none"
+                                                    type="text"
+                                                    id="fullName"
+                                                    name="fullName"
+                                                />
+                                            </div>
+                                            <div className="w-10/12">
+                                                <label
+                                                    className="block text-gray-700 dark:text-zinc-300 text-md font-bold mb-2 tracking-widest"
+                                                    htmlFor="status"
+                                                >
+                                                    Status
+                                                </label>
+                                                <select
+                                                    name="status"
+                                                    onChange={({ target }) => setFieldValue("status", target.value)}
+                                                    className="bg-zinc-100 shadow appearance-none border rounded-xl h-[40px] py-2 px-3 text-gray-700 dark:text-zinc-200 leading-tight focus:outline-none focus:shadow-outline w-full dark:bg-zinc-800 dark:border-none"
+                                                    value={values.status}
+                                                >
+                                                    <option key={1} value="active">active</option>
+                                                    <option key={2} value="inactive">inactive</option>
+                                                </select>
+                                            </div>
+                                            <div className="w-10/12">
+                                                <label
+                                                    className="block text-gray-700 dark:text-zinc-300 text-md font-bold mb-2 tracking-widest"
+                                                    htmlFor="Email"
+                                                >
+                                                    Email
+                                                </label>
+                                                <Field
+                                                    className="bg-zinc-100 shadow appearance-none border rounded-xl h-[40px] w-full py-2 px-3 text-gray-700 dark:text-zinc-300  leading-tight focus:outline-none focus:shadow-outline dark:bg-zinc-800 dark:border-none"
+                                                    type="email"
+                                                    id="Email"
+                                                    name="email"
+                                                />
+                                            </div>
+                                            <div className="w-10/12">
+                                                <label
+                                                    className="block text-gray-700 dark:text-zinc-300 text-md font-bold mb-2 tracking-widest"
+                                                    htmlFor="password"
+                                                >
+                                                    Password
+                                                </label>
+                                                {isPassordGenerated ? (
+                                                    <Field
+                                                        className="bg-zinc-100 shadow appearance-none border rounded-xl h-[40px] w-full py-2 px-3 text-gray-700 dark:text-zinc-300  leading-tight focus:outline-none focus:shadow-outline dark:bg-zinc-800 dark:border-none select-none"
+                                                        type="text"
+                                                        disabled
+                                                        id="password"
+                                                        name="temporaryPassword"
+                                                    />
+                                                ) : (
+                                                    <Button
+                                                        className='bg-sky-400 text-zinc-50 dark:text-zinc-100 dark:bg-sky-600 tracking-widest w-full font-semibold'
+                                                        onClick={() => handleGeneratePassword(setFieldValue)}
+                                                    >
+                                                        Generate Temporary Passord
+                                                    </Button>
+                                                )}
+                                            </div>
+                                            
+                                            <div className="w-10/12 ">
 
-                                               <Button
-                                                   type="submit"
-                                                   className="bg-[#40C48E] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
-                                               >
-                                                   Create
-                                               </Button>
-                                           </div>
-                                       </Form>
-                                   )}
-                               </Formik>
-                           </div>
-                       </ModalBody>
-                       <ModalFooter>
-                           <Button color="danger" variant="light" onPress={onClose}>
-                               Cancel
-                           </Button>
-                       </ModalFooter>
-                   </>
-               )}
-           </ModalContent>
+                                                <Button
+                                                    type="submit"
+                                                    className="bg-[#40C48E] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full tracking-widest"
+                                                >
+                                                    Create
+                                                </Button>
+                                            </div>
+                                        </Form>
+                                    )}
+                                </Formik>
+                            </div>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button color="danger" variant="light" onPress={onClose}>
+                                Cancel
+                            </Button>
+                        </ModalFooter>
+                    </>
+                )}
+            </ModalContent>
         </Modal>
     )
 }
