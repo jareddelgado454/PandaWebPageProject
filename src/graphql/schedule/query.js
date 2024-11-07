578998b78a662f40bbce7e03d5e67a6144defa81
DelioCoder <davideveriwhere@gmail.com>
2024-11-07 14:25:49 -0500

    Retrieve scheduled services

--- src/graphql/schedule/query.js
+++ src/graphql/schedule/query.js
@@ -1,10 +1,27 @@
 import { gql } from "graphql-tag";
+
+export const retrieveCustomerScheduledServices = gql`
+    query MyQuery($customerId: ID!) {
+        listScheduledServices(filter: {customerId: {eq: $customerId}}){
+            items{
+                id
+                status
+                price
+                type
+                title
+                createdAt
+            }
+        }
+    }
+`;
+
 export const retrieveScheduledServicesByTechnicianId = gql`
     query MyQuery($technicianOfferedId: ID!) {
         listScheduledServices(filter: {technicianOfferedId: {eq: $technicianOfferedId}, status: {eq: "pending"}}) {
             items {
                 id
-                dateScheduled
+                dateStartScheduled
+                dateEndScheduled
             }
         }
     }
