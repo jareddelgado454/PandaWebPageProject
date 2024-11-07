35c886553b31d82b0f6e21470bd615243e516ea5
DelioCoder <davideveriwhere@gmail.com>
2024-11-07 14:26:21 -0500

    endDate added

--- src/components/customer/modals/ScheduledModal.jsx
+++ src/components/customer/modals/ScheduledModal.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 import { Modal, ModalContent, ModalBody, DateInput, Button } from "@nextui-org/react";
 import { parseAbsoluteToLocal } from '@internationalized/date';
 const ScheduledModal = ({ isOpen, onOpenChange, dates, setDates, setCurrentStep }) => {
-    const handleDateChange = (field, dateObject) => {
+    const handleDateChange = (dateObject) => {
         const { year, month, day, hour, minute, second } = dateObject;
 
-        const date = new Date(
+        const startDate = new Date(
             year,
             month - 1,
             day,
@@ -14,7 +14,9 @@ const ScheduledModal = ({ isOpen, onOpenChange, dates, setDates, setCurrentStep 
             minute,
             second || 0
         );
-        setDates((prevState) => ({ ...prevState, [field]: date }));
+
+        const endDate = new Date(startDate.getTime() + 30 * 60000);
+        setDates({ start: startDate, end: endDate });
     };
     return (
         <Modal backdrop='blur' isOpen={isOpen} onOpenChange={onOpenChange} placement='center' size="5xl" className='min-h-[14rem]'>
@@ -27,16 +29,11 @@ const ScheduledModal = ({ isOpen, onOpenChange, dates, setDates, setCurrentStep 
                                 <div className='flex gap-8'>
                                     <DateInput
                                         label={"Schedule Start Date"}
-                                        granularity="minute"
+                                        granularity='minute'
+                                        hideTimeZone
                                         defaultValue={parseAbsoluteToLocal(dates.start.toISOString())}
-                                        onChange={(e) => handleDateChange("start", e)}
+                                        onChange={(e) => handleDateChange(e)}
                                     />
-                                    {/* <DateInput
-                                        label={"Schedule End Date"}
-                                        granularity="minute"
-                                        defaultValue={parseAbsoluteToLocal(dates.start.toISOString())}
-                                        onChange={(e) => handleDateChange("end", e)}
-                                    /> */}
                                 </div>
                                 <Button onClick={() => { setCurrentStep(3); onclose(); }} color='success' className='text-zinc-100 tracking-widest font-semibold'>
                                     Next Step
